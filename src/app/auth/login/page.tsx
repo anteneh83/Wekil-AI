2a900b8c1c9fb387bb1848bdbd15cb6d941bd338
nahomhailu12 <hailunahom47@gmail.com>
2025-08-24 23:23:54 +0300

    icons are now added and next auth is now embeded

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
 
 interface formdata {
   email: string;
   password: string;
 }
 
-const formSubmitHandler = async (data: formdata) => {};
+const formSubmitHandler = async (data: formdata) => {
+  try {
+    const response = await signIn("credentials", {
+      email: data.email,
+      password: data.password,
+      redirect: false,
+    });
+    if (!response?.ok) {
+      throw new Error("Login failed");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 const Login = () => {
   const form = useForm<formdata>({
     mode: "onBlur",
@@ -31,18 +46,25 @@ const Login = () => {
             <label htmlFor="email" className="font-extrabold">
               Email address:
             </label>
-            <input
-              className="block border bg-gray-200 shadow-gray-100 border-gray-200 my-2 p-2 h-8 rounded-md w-full focus:border-violet-600 "
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              {...register("email", {
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Invalid email address",
-                },
-              })}
-            />
+            <div className="flex justify-between">
+              <img
+                src="https://www.citypng.com/public/uploads/preview/hd-letter-email-round-blue-icon-transparent-png-70175169503463700jpjirgdw.png"
+                className="w-8 h-8 mt-2 mx-2 rounded-full "
+                alt=""
+              />
+              <input
+                className="inline-block border placeholder: text-blue-800 bg-gray-200 shadow-gray-100 border-gray-200 my-2 p-2 h-8 rounded-md w-full focus:outline-none focus:border-violet-600 "
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                {...register("email", {
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+              />
+            </div>
             {errors.email && (
               <p className="text-red-600 text-bold text-sm">
                 {errors.email?.message}
@@ -53,19 +75,26 @@ const Login = () => {
             <label htmlFor="password" className="font-extrabold">
               Password:{" "}
             </label>
-            <input
-              {...register("password", {
-                minLength: {
-                  value: 8,
-                  message: "Invalid password",
-                },
-              })}
-              className="block border bg-gray-200 shadow-gray-100 border-gray-200 my-2 p-2 h-8 rounded-md w-full focus:border-violet-600"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-            />
+            <div className="flex justify-between">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi_-QMHfYRJLbTpVwry2P9ngVKQlWVqHChVw&s"
+                className="w-8 h-8 mt-2 mx-2 rounded-full"
+                alt=""
+              />
+              <input
+                {...register("password", {
+                  minLength: {
+                    value: 8,
+                    message: "Password must be atleast 8 character long",
+                  },
+                })}
+                className="inline-block border bg-gray-200 shadow-gray-100 border-gray-200 my-2 p-2 h-8 rounded-md w-full focus:outline-none focus:border-violet-400"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+            </div>
             {errors.password && (
               <p className="text-red-600 text-bold text-sm">
                 {errors.password?.message}
@@ -84,8 +113,15 @@ const Login = () => {
           <span className="px-3 bg-white">or continue with</span>
           <div className="flex-grow border-t mt-3 border-gray-300"></div>
         </div>
-
-        <button className="w-full rounded-lg my-4 font-bold border border-gray-200 text-center py-1">
+        <button
+          className="w-full flex items-center justify-center gap-2 rounded-lg my-4 text-green-700 font-bold border border-gray-200 py-2"
+          onClick={() => signIn("google")}
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Google_Favicon_2025.svg"
+            alt="google"
+            className="w-5 h-5"
+          />
           Continue with Google
         </button>
       </div>
